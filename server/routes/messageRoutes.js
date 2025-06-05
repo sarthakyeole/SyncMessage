6efc064dab886decbb89fdc2f762521cd8e5e754
sarthakyeole <yeolesarthakdev@gmail.com>
2025-06-05 15:36:53 +0530

    errors resolved in server folder

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import { protectRoute } from '../middleware/auth';
-import { getMessages, getUserForSiderbar, markMessageAsSeen, sendMessage } from '../controllers/messageController';
-import { get } from 'mongoose';
-import { checkAuth } from '../middleware/auth';
-import User from '../models/User.js';
-import Message from '../models/Message.js';
+import express from "express"
+import { protectRoute } from "../middleware/auth.js"
+import {
+  getMessages,
+  getUserForSiderbar,
+  markMessageAsSeen,
+  sendMessage,
+} from "../controllers/messageController.js"
 
+const messageRouter = express.Router()
 
-const messageRouter = express.Router();
+messageRouter.get("/users", protectRoute, getUserForSiderbar)
+messageRouter.get("/:id", protectRoute, getMessages)
+messageRouter.put("/:id", protectRoute, markMessageAsSeen)
+messageRouter.post("/send/:id", protectRoute, sendMessage)
 
-messageRouter.get("/users", protectRoute, getUserForSiderbar );
-messageRouter.get("/:id", protectRoute, getMessages);
-messageRouter.put("/:id", protectRoute, markMessageAsSeen);
-messageRouter.post("/send/:id", protectRoute, sendMessage);
-
-export default messageRouter;
-
+export default messageRouter
