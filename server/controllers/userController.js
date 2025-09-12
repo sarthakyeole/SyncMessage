c8531f4f46a1a632f453cef34f420c3b0f3daa12
sarthakyeole <yeolesarthakdev@gmail.com>
2025-09-12 18:37:35 +0530

    fix: fixed minor formatting issue

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,10 +5,10 @@ import cloudinary from "../lib/cloudinary.js";
 
 // Signup a new user
 export const signup = async (req, res) => {
-    const { fullname, email, password, bio } = req.body;
+    const { fullName, email, password, bio } = req.body;
 
     try{
-        if(!fullname || !email || !password || !bio) {
+        if(!fullName || !email || !password || !bio) {
             return res.json({success: false, message: "Missing Details" })
         }
         const user = await User.findOne({email});
@@ -20,7 +20,7 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = await User.create({
-            fullname,
+            fullName,
             email,
             password: hashedPassword,
             bio
